4c7b0e1847a804df742728527046e3fa0b640b66
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-01-21 21:34:50 +0100

    Moved form into first tab only so the submit button does not show in the solves tab

--- src/components/challenges/ChallengeModal.js
+++ src/components/challenges/ChallengeModal.js
@@ -25,7 +25,7 @@ export default function ChallengeModal({ isOpen, onClose }) {
 
         >
         <ModalOverlay />
-        <ModalContent maxH="100%">
+        <ModalContent maxH="100%" minH="400px">
             <ModalCloseButton />
             <ModalBody margin="0" padding="0">
                 <Tabs variant='enclosed'>
@@ -35,23 +35,70 @@ export default function ChallengeModal({ isOpen, onClose }) {
                 </TabList>
 
                 <TabPanels >
-                        <TabPanel maxH="600px" overflowX="auto" marginRight="3px" padding="0px 40px 0px 45px">
-                            <Flex w="100%" flexDir="column" fontFamily="Audiowide" fontSize="20px">
-                                <Box w="100%" marginTop="15px">
-                                    <Center>
-                                        <Text textAlign="center">{selectedChallenge.name}</Text>
-                                    </Center>
-                                </Box>
-                                <Box w="100%" marginTop="10px" marginBottom="45px">
-                                    <Center>
-                                        <Text>{selectedChallenge.points}</Text>
-                                    </Center>
-                                </Box>
+                        <TabPanel>
+                            <Flex maxH="600px" overflowX="auto" marginRight="3px" padding="0px 40px 0px 45px">
+                                <Stack w="100%">
+                                    <Flex w="100%" flexDir="column" fontFamily="Audiowide" fontSize="20px">
+                                        <Box w="100%" marginTop="15px">
+                                            <Center>
+                                                <Text textAlign="center">{selectedChallenge.name}</Text>
+                                            </Center>
+                                        </Box>
+                                        <Box w="100%" marginTop="10px" marginBottom="45px">
+                                            <Center>
+                                                <Text>{selectedChallenge.points}</Text>
+                                            </Center>
+                                        </Box>
+                                    </Flex>                                
+                                    <Flex className='markdown-body'>
+                                        <div dangerouslySetInnerHTML={{ __html: selectedChallenge.description}} />                                        
+                                    </Flex>
+                                </Stack>
                             </Flex>
-                            <Flex className='markdown-body'>
-                                <div dangerouslySetInnerHTML={{ __html: selectedChallenge.description}} />                                        
-                            </Flex>
-                            
+                            <form onSubmit={submitForm}>
+                                <ModalFooter>
+                                    <Flex w="100%">
+                                        <Stack w="100%">
+                                            <FormControl marginBottom="20px">
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<FaFlag color='#7e8b9f'/>}
+                                                    />
+                                                    <Input 
+                                                        disabled={!selectedChallenge.solved ? false : true} 
+                                                        size="lg" 
+                                                        borderBottomWidth="2px" 
+                                                        variant='flushed' 
+                                                        type="text" 
+                                                        name="name" 
+                                                        placeholder="HKN{some-flag-here}" />
+                                                </InputGroup>
+                                            </FormControl>
+                                            <Button 
+                                                w="100%"
+                                                type='submit' 
+                                                backgroundColor="#54616e"
+                                                _hover={{backgroundColor: '#434d56'}}
+                                                color="#dfdfe3"
+                                                variant='solid'
+                                                disabled={!selectedChallenge.solved ? false : true}
+                                            >
+                                                {!selectedChallenge.solved ? 
+                                                <>
+                                                    Submit
+                                                </>
+                                                : 
+                                                <>
+                                                    Already Solved
+                                                </>
+                                                }
+                                            </Button>
+                                        </Stack>
+                                    </Flex>
+                                    
+                                </ModalFooter>
+                            </form>  
                         </TabPanel>
                         <TabPanel maxH="600px" overflowX="auto" marginRight="3px" padding="0px 40px 0px 45px">
                         <p>two!</p>
@@ -59,43 +106,7 @@ export default function ChallengeModal({ isOpen, onClose }) {
                     </TabPanels>
                 </Tabs>                      
             </ModalBody>
-            <form onSubmit={submitForm}>
-                <ModalFooter>
-                    <Flex w="100%">
-                        <Stack w="100%">
-                            <FormControl marginBottom="20px">
-                                <InputGroup>
-                                    <InputLeftElement
-                                        pointerEvents="none"
-                                        children={<FaFlag color='#7e8b9f'/>}
-                                    />
-                                    <Input size="lg" borderBottomWidth="2px" variant='flushed' type="text" name="name" placeholder="HKN{some-flag-here}" />
-                                </InputGroup>
-                            </FormControl>
-                            <Button 
-                                w="100%"
-                                type='submit' 
-                                backgroundColor="#54616e"
-                                _hover={{backgroundColor: '#434d56'}}
-                                color="white"
-                                variant='solid'
-                                disabled={!selectedChallenge.solved ? false : true}
-                            >
-                                {!selectedChallenge.solved ? 
-                                <>
-                                    Submit
-                                </>
-                                : 
-                                <>
-                                    Already Solved
-                                </>
-                                }
-                            </Button>
-                        </Stack>
-                    </Flex>
-                    
-                </ModalFooter>
-            </form>  
+            
                       
         </ModalContent>
         </Modal>
