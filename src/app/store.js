d888f76bbaad16ac4a0cf1b531c7806ee5b5e46d
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-02-02 20:40:59 +0100

    removed code from webclient

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import teamReducer from '../features/teams/teamSlice'
 import eventReducer from '../features/events/eventSlice'
 import exerciseReducer from '../features/exercises/exerciseSlice'
-import orgReducer from '../features/organizations/organizationSlice'
-import agentReducer from '../features/agents/agentSlice'
 import genericReducer from '../features/generic/genericSlice'
 
 
@@ -12,8 +10,6 @@ const store = configureStore({
         team: teamReducer,
         event: eventReducer,
         exercise: exerciseReducer,
-        org: orgReducer,
-        agent: agentReducer,
         generic: genericReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: {ignoredActionPaths: ['payload.config', 'payload.request']}}),
