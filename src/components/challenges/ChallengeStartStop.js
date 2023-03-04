abd6312aa521a2f76f0b47f7394c29c33cf107c0
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-03-04 01:25:53 +0100

    Start implementing start, stop and reset button

--- src/components/challenges/ChallengeStartStop.js
+++ src/components/challenges/ChallengeStartStop.js
@@ -1,16 +1,33 @@
 import { Button } from '@chakra-ui/react'
 import React from 'react'
+import { useSelector } from 'react-redux';
 
-function ChallengeStartStop() {
+function ChallengeStartStop({parentExerciseTag}) {
+  const loggedInTeam = useSelector((state) => state.team.loggedInTeam);
   return (
-    <Button 
-        backgroundColor="#54616e"
-        _hover={{ backgroundColor: "#434d56" }}
-        color="#dfdfe3"
-        variant="solid"
-    >
-        Start
-    </Button>
+    <>
+    {typeof loggedInTeam.lab.labInfo.exercisesStatus[
+      parentExerciseTag
+    ] === "undefined" ? (
+        <Button 
+          backgroundColor="#54616e"
+          _hover={{ backgroundColor: "#434d56" }}
+          color="#dfdfe3"
+          variant="solid"
+        >
+            Start
+        </Button>
+    ) : (
+      <Button 
+          backgroundColor="#54616e"
+          _hover={{ backgroundColor: "#434d56" }}
+          color="#dfdfe3"
+          variant="solid"
+        >
+            Stop
+        </Button>
+    )}
+    </>
   )
 }
 
