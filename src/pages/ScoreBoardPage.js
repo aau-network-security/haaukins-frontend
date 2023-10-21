294abca97bec16098c1fcecd0e84aaa4cdbbda53
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-10-21 14:18:19 +0200

    Added autorefresh enabler as it interferes with score selectors

--- src/pages/ScoreBoardPage.js
+++ src/pages/ScoreBoardPage.js
@@ -1,4 +1,4 @@
-import { Box, Center, Flex, Text, Icon } from "@chakra-ui/react";
+import { Box, Center, Flex, Text, Icon, Select, Checkbox } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { SlGraph } from "react-icons/sl";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,13 +13,26 @@ export default function ScoreBoardPage() {
     dispatch(fetchScores(eventinfo));
   }, [dispatch]);
 
+  const [time, setTime] = useState(Date.now());
+  const [autoRefreshEnabled, setAutoRefreshEnabled] = useState(false)
+  const [autoRefreshInterval, setAutoRefreshInterval] = useState(5000)
   useEffect(() => {
-    const interval = setInterval(() => dispatch(fetchScores(eventinfo)), 5000);
-    dispatch(fetchScores(eventinfo));
+    const interval = setInterval(() => setTime(Date.now()), autoRefreshInterval);
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [autoRefreshInterval]);
+
+  useEffect(() => {
+    if (autoRefreshEnabled) {
+      dispatch(fetchScores(eventinfo))
+    }
+  }, [time, autoRefreshEnabled])
+
+  const onCheckChange = (e) => {
+    console.log(e)
+    setAutoRefreshEnabled(e.target.checked)
+  }
 
   return (
     <Flex maxW={"1140px"} w="100%" m={"auto"} flexDir="column" color="#211a52">
@@ -29,6 +42,24 @@ export default function ScoreBoardPage() {
           <Text marginLeft="20px">Scoreboard</Text>
         </Center>
       </Box>
+      <Text><b>Auto Refresh</b></Text>
+      <Flex>
+      <Select 
+        defaultValue={5} 
+        width="200px" 
+        marginRight="10px"
+        onChange={(event) => setAutoRefreshInterval(event.target.value)}
+        backgroundColor="aau.bg"
+      >
+        <option value={5000}>5 Seconds</option>
+        <option value={10000}>10 Seconds</option>
+        <option value={15000}>15 Seconds</option>
+        <option value={20000}>20 Seconds</option>
+        <option value={25000}>25 Seconds</option>
+        <option value={30000}>30 Seconds</option>
+      </Select>
+      <Checkbox value={autoRefreshEnabled} onChange={onCheckChange}>Enabled</Checkbox>
+      </Flex>
       <Box h="500px">
         <ScoreGraph/>
       </Box>
